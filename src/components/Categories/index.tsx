d3b955a19fab321d156ddaea338f3d7945e2a919
Marcos Colella <colella.dev@gmail.com>
2025-08-16 19:20:21 -0300

    refactor: Categories Icons

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,36 +1,68 @@
 import styles from './styles.module.scss';
-import { useState } from 'react';
-import { Icon } from '../../ui/icons/Icon';
 
-const categoriesItems = [
-    { text: "Tecnologia", icon: 'tecnology'},
-    { text: "Supermercado", icon: 'supermarket'},
-    { text: "Bebidas", icon: 'drinks'},
-    { text: "Ferramentas", icon: 'tools'},
-    { text: "Saúde", icon: 'health'},
-    { text: "Esportes e Fitness", icon: 'fitness'},
-    { text: "Moda", icon: 'fashion'}
-]
+import drinks from '../../assets/icons/drinks.svg';
+import fashion from '../../assets/icons/fashion.svg'
+import fitness from '../../assets/icons/fitness.svg';
+import health from '../../assets/icons/health.svg';
+import supermarket from '../../assets/icons/supermarket.svg';
+import tecnology from '../../assets/icons/tecnology.svg';
+import tools from '../../assets/icons/tools.svg';
 
 export function Categories() {
-    const [selectedItem, setSelectedItem] = useState('Tecnologia');
-
     return (
         <section className={styles.categoriesContainer}>
-                <div className={styles.categoriesGroup}>
-                {categoriesItems.map((item) => (
-                     <div 
-                      key={item.text} 
-                      className={`${styles.categories} ${selectedItem === item.text ? styles.selected : ''}`}
-                      onClick={() => setSelectedItem(item.text)}
-                     >
-                        <div className={styles.iconDiv}>
-                            {item.icon && (<Icon name={item.icon} width={70} height={69}/>)}
-                        </div>
-                        {item.text}
+            <div className={styles.categoriesGroup}>
+
+                <div className={styles.categoriesActive}>
+                    <div className={styles.iconDivActive}>
+                        <img src={tecnology}  alt="Imagem de tecnologia"/>
                     </div>
-                ))}
+                     Tecnologia
                 </div>
+
+                <div className={styles.categories}>
+                    <div className={styles.iconDiv}>
+                        <img src={supermarket}  alt="Imagem"/>
+                    </div>
+                     Supermercado
+                </div>
+
+                <div className={styles.categories}>
+                    <div className={styles.iconDiv}>
+                        <img src={drinks}  alt="Imagem "/>
+                    </div>
+                     Bebidas
+                </div>  
+
+                 <div className={styles.categories}>
+                    <div className={styles.iconDiv}>
+                        <img src={tools}  alt="Imagem "/>
+                    </div>
+                     Ferramentas
+                </div>  
+
+                 <div className={styles.categories}>
+                    <div className={styles.iconDiv}>
+                        <img src={health}  alt="Imagem "/>
+                    </div>
+                     Saúde
+                </div>  
+
+                <div className={styles.categories}>
+                    <div className={styles.iconDiv}>
+                        <img src={fitness}  alt="Imagem "/>
+                    </div>
+                     Esportes e Fitness
+                </div>  
+
+                <div className={styles.categories}>
+                    <div className={styles.iconDiv}>
+                        <img src={fashion}  alt="Imagem "/>
+                    </div>
+                     Moda
+                </div>  
+
+            </div>
         </section>
     )
 }
