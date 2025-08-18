4d3f70bb6e70538599a7b619b8018e589acfcb8c
Marcos Colella <colella.dev@gmail.com>
2025-08-18 20:18:35 -0300

    feat: Slider Function

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,5 +1,5 @@
 import styles from './styles.module.scss';
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { fetchProducts } from "../../services/apiProducts";
 import { ProductCard } from "./ProductCard";
 import { ProductModal } from "./ProductModal"
@@ -8,6 +8,7 @@ import type { ProductTypes } from "../../types/product";
 export function Slider() {
     const [products, setProducts] = useState<ProductTypes[]>([]);
     const [selectedProduct, setSelectedProduct] = useState<ProductTypes | null>(null);
+    const sliderRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
     async function getProducts() {
@@ -30,17 +31,35 @@ export function Slider() {
         setSelectedProduct(null);
     };
 
+    const handleNext = () => {
+    if (sliderRef.current) {
+      const width = sliderRef.current.offsetWidth; 
+      sliderRef.current.scrollBy({ left: width, behavior: "smooth" });
+    }
+  };
+
+  const handlePrev = () => {
+    if (sliderRef.current) {
+      const width = sliderRef.current.offsetWidth; 
+      sliderRef.current.scrollBy({ left: -width, behavior: "smooth" });
+    }
+  };
+
     return (
         <>
-            <div className={styles.sliderContainer}>
-                {products.map((product) => (
+          <div className={styles.sliderWrapper}>
+            <button className={styles.navButton} onClick={handlePrev}>‹</button>
+             <div className={styles.sliderContainer} ref={sliderRef}>
+               {products.map((product) => (
                 <ProductCard key={product.productName} product={product} onClick={handleOpenModal} />
-                ))}
-            </div>
+               ))}
+             </div>
+            <button className={styles.navButton} onClick={handleNext}>›</button>
+         </div>
             
-            {selectedProduct && (
-                <ProductModal product={selectedProduct} onClose={handleCloseModal} />
-            )}
+          {selectedProduct && (
+            <ProductModal product={selectedProduct} onClose={handleCloseModal} />
+          )}
         </>
     )
 }
