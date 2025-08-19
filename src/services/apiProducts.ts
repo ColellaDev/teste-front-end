151d898d423240c6622309762b8a9e6cb9907fd6
Marcos Colella <colella.dev@gmail.com>
2025-08-19 09:04:02 -0300

    refactor: API

--- src/services/apiProducts.ts
+++ src/services/apiProducts.ts
@@ -1,7 +1,18 @@
+import type { ProductTypes } from '../types/product';
 
+const API_URL = import.meta.env.VITE_API_URL as string;
 
-export async function fetchProducts() {
-  const response = await fetch("http://localhost:4000/produtos");
-  const data = await response.json();
-  return data.products;
+export async function fetchProducts(): Promise<ProductTypes[]> {
+  if (!API_URL) {
+    throw new Error('VITE_API_URL não definida');
+  }
+
+  const res = await fetch(API_URL);
+
+  if (!res.ok) {
+    throw new Error(`Erro ao buscar produtos: ${res.status} ${res.statusText}`);
+  }
+
+  const data = await res.json();
+  return data.products as ProductTypes[];
 }
